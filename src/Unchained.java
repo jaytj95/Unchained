b8d2c7d520fb06ba658f85931a255d881abed340
Jason John <jaytj95@gmail.com>
2015-09-18 05:49:09 -0400

    Placeholder

--- src/Unchained.java
+++ src/Unchained.java
@@ -1,4 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 import fi.foyt.foursquare.api.FoursquareApi;
 import fi.foyt.foursquare.api.FoursquareApiException;
@@ -14,13 +19,21 @@ public class Unchained {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter lat,lng");
 		String ll = keyboard.next();
+		keyboard.close();
 		new Thread(new Runnable() {
 			
 			@Override
 			public void run() {
 				try {
-					getVenues(ll);
+					ArrayList<String> venues = 
+							curateRestaurants(getVenues(ll), loadChainRestaurants());
+					for(String v : venues) {
+						System.out.println(v);
+					}
 				} catch (FoursquareApiException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (FileNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
@@ -30,24 +43,49 @@ public class Unchained {
 	}
 
 
-	public static void getVenues(String ll) throws FoursquareApiException {
+	public static ArrayList<String> getVenues(String ll) throws FoursquareApiException {
+		ArrayList<String> result = new ArrayList<>();
 		FoursquareApi foursquareApi = new FoursquareApi(CLIENT_ID, CLIENT_SECRET, "www.google.com");
 		foursquareApi.setVersion("20150917");
 		// After client has been initialized we can make queries.
-		Result<VenuesSearchResult> result = foursquareApi.venuesSearch(ll, null, null, null, null, 50, null, CATEGORY_RESTAURANTS, null, null, null, 20000, null);
+		Result<VenuesSearchResult> fsResult = foursquareApi.venuesSearch(ll, null, null, null, null, 50, null, CATEGORY_RESTAURANTS, null, null, null, 20000, null);
 
-		if (result.getMeta().getCode() == 200) {
-			// if query was ok we can finally we do something with the data
-			for (CompactVenue venue : result.getResult().getVenues()) {
-				// TODO: Do something we the data
-				System.out.println(venue.getName());
+		if (fsResult.getMeta().getCode() == 200) {
+			for(CompactVenue v: fsResult.getResult().getVenues()) {
+				result.add(v.getName());
 			}
 		} else {
 			// TODO: Proper error handling
 			System.out.println("Error occured: ");
-			System.out.println("  code: " + result.getMeta().getCode());
-			System.out.println("  type: " + result.getMeta().getErrorType());
-			System.out.println("  detail: " + result.getMeta().getErrorDetail()); 
+			System.out.println("  code: " + fsResult.getMeta().getCode());
+			System.out.println("  type: " + fsResult.getMeta().getErrorType());
+			System.out.println("  detail: " + fsResult.getMeta().getErrorDetail()); 
 		}
+		Set setItems = new LinkedHashSet(result);
+		result.clear();
+		result.addAll(setItems);
+		return result;
+	}
+	
+	public static ArrayList<String> loadChainRestaurants() throws FileNotFoundException {
+		Scanner s = new Scanner(new File("files/chains.txt"));
+		ArrayList<String> chains = new ArrayList<String>();
+		while (s.hasNextLine()){
+		    chains.add(s.nextLine());
+		}
+		s.close();
+		return chains;
+	}
+	
+	public static ArrayList<String> curateRestaurants(ArrayList<String> foursquare, ArrayList<String> chain) {
+		ArrayList<String> nonChains = new ArrayList<>();
+		for(String venue : foursquare) {
+			for(String chainRestaurant : chain) {
+				if(!venue.contains(chainRestaurant)) {
+					nonChains.add(venue);
+				}
+			}
+		}
+		return nonChains;
 	}
 }
